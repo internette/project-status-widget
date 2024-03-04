6f3a18c7b9e6c2bfbeb5262ccfa4883ec78be34f
Antoinette Janus <antoinettejanus@lululemon.com>
2024-03-04 11:19:30 -0800

    Removed console logs

--- src/react/components/sign-in-button/sign-in-button.js
+++ src/react/components/sign-in-button/sign-in-button.js
@@ -1,7 +1,5 @@
 const SignInButton = ({signinType}) => {
-    console.log(signinType)
     const { provider, callback } = signinType;
-    console.log(provider);
     return <button onClick={()=> callback}>{provider}</button>
 }
 
