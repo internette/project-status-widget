32c4526312d8713a0980581e5bb31dbe28324dce
Antoinette Janus <acj.gfx@gmail.com>
2024-03-10 10:49:20 -0500

    List prs (#5)
    * Setup
    * Listing PRs

--- src/react/App.js
+++ src/react/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import SignInButtons from "./components/sign-in-buttons/sign-in-buttons";
+import PrList from "./components/pr-list/pr-list";
 
 function App() {
+  const [authToken, setAuthToken] = useState("");
+  const [prs, setPrs] = useState([]);
   return (
     <div className="App">
-      <SignInButtons />
+      {authToken.length > 0 ? (
+        <PrList prs={prs} />
+      ) : (
+        <SignInButtons setAuthToken={setAuthToken} setPrs={setPrs} />
+      )}
     </div>
   );
 }
