ecde391455e32fefa9fc577a7bceffa3b940b66a
Antoinette <acj.gfx@gmail.com>
2024-03-09 18:17:40 -0600

    Created buttons and wired up

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,53 +1,122 @@
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow, ipcMain } = require("electron/main");
+const path = require("node:path");
+const { CLIENT_ID } = require("../constants.js");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+let githubAuthWindow;
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000');
+  // and load the index.html of the app.
+  mainWindow.loadURL("http://localhost:3000");
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    })
+  // Emitted when the window is closed.
+  mainWindow.on("closed", function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+app.on("window-all-closed", function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow()
-    }
+app.whenReady().then(() => {
+  ipcMain.handle("github-login", () => {
+    const options = {
+      client_id: CLIENT_ID,
+      scopes: ["user:email", "notifications"], // Scopes limit access for OAuth tokens.
+    };
+    githubAuthWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      show: false,
+      webPreferences: {
+        preload: path.join(__dirname, "preload.js"),
+      },
+    });
+    const state = require("crypto").randomBytes(16).toString("hex");
+    const githubUrl = "https://github.com/login/oauth/authorize?";
+    const authUrl =
+      githubUrl +
+      "client_id=" +
+      options.client_id +
+      "&state=" +
+      state +
+      "&login";
+    githubAuthWindow.loadURL(authUrl);
+    githubAuthWindow.show();
+    githubAuthWindow.webContents.openDevTools();
+
+    const handleCallback = (url) => {
+      // If there is a code, proceed to get token from github
+      const queryString = url.split("callback")[1];
+      const urlParams = new URLSearchParams(queryString);
+      const code = urlParams.get("code");
+      const error = urlParams.get("error");
+      if (code) {
+        const { net } = require("electron");
+        githubAuthWindow.destroy();
+        const tokenUrl =
+          "https://project-status-widget-api.vercel.app/api/get-token?authToken=" +
+          code;
+        const request = net.request(tokenUrl);
+        let oAuthQuery = "";
+        request.on("response", (response) => {
+          response.on("data", (chunk) => {
+            oAuthQuery += chunk;
+          });
+          response.on("end", () => {
+            oAuthQuery = oAuthQuery.split(":")[0];
+            const oauthResParams = new URLSearchParams(oAuthQuery);
+            const accessToken = oauthResParams.get("access_token");
+            mainWindow.webContents.send("set-gh-access-token", {
+              access_token: accessToken,
+            });
+          });
+        });
+        request.end();
+      } else if (error) {
+        alert(
+          "Oops! Something went wrong and we couldn't" +
+            "log you in using Github. Please try again."
+        );
+      }
+    };
+
+    githubAuthWindow.webContents.on("did-navigate", function (event, url) {
+      handleCallback(url);
+    });
+    githubAuthWindow.webContents.on(
+      "did-get-redirect-request",
+      function (event, oldUrl, newUrl) {
+        handleCallback(newUrl);
+      }
+    );
+    // Reset the authWindow on close
+    githubAuthWindow.on("closed", () => {
+      githubAuthWindow = null;
+    });
+  });
+  createWindow();
 });
---
+++ src/react/components/sign-in-buttons/sign-in-buttons.js
@@ -0,0 +1,65 @@
+import { useEffect } from "react";
+import { Octokit } from "octokit";
+import SignInButton from "../sign-in-button/sign-in-button";
+
+const AllSignInButtons = () => {
+  useEffect(() => {
+    window.ghLogin.receive((event, args) => {
+      ghCallback(args);
+    });
+  }, []);
+  const ghCallback = async ({ access_token }) => {
+    const octokit = new Octokit({
+      auth: access_token,
+    });
+    const userResp = await octokit.request("GET /user", {
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+    const username = await userResp.data.login;
+    if (username) {
+      const searchQueryParams = `is:open is:pr involves:${username}`;
+      const searchQuery = "?q=" + encodeURIComponent(searchQueryParams);
+      const response = await octokit.request(
+        "GET /search/issues" + searchQuery,
+        {
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+        }
+      );
+      console.log(response);
+    }
+  };
+  const ghClickHandler = () => {
+    window.ghLogin.send();
+  };
+  const glCallback = () => {
+    console.log("placeholder");
+  };
+  const glClickHandler = () => {
+    console.log("placeholder");
+  };
+  const signinTypes = [
+    {
+      provider: "github",
+      callback: ghCallback,
+      onclickHandler: ghClickHandler,
+    },
+    {
+      provider: "gitlab",
+      callback: glCallback,
+      onclickHandler: glClickHandler,
+    },
+  ];
+  return (
+    <div>
+      {signinTypes.map((signinType) => (
+        <SignInButton signinType={signinType} />
+      ))}
+    </div>
+  );
+};
+
+export default AllSignInButtons;
