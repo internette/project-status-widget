f828b129c5ac867ed90bb6cc81acad130402e1fe
Antoinette <acj.gfx@gmail.com>
2024-03-14 10:46:43 -0700

    Added icon

--- src/react/components/pr-item/pr-item.js
+++ src/react/components/pr-item/pr-item.js
@@ -4,8 +4,16 @@ import styles from "./pr-item.module.scss";
 const PrLineItem = ({ prDetails }) => {
   const { provider, linkAddress, prName, state } = prDetails;
   return (
-    <li className={cs(styles[provider], styles.lineItem)}>
+    <li className={cs(styles[`${provider}Link`], styles.lineItem)}>
       <a href={linkAddress} className={cs(styles[state], styles.lineItemLink)}>
+        <i
+          className={cs(
+            "fa-solid",
+            "fa-brands",
+            `fa-${provider}`,
+            styles.listIcon
+          )}
+        ></i>
         <strong>
           <span>{prName}</span>
         </strong>
