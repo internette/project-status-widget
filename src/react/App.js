ec58ca11fff4f69b32198df47fff100d57866cdd
Antoinette Janus <antoinettejanus@lululemon.com>
2024-03-19 13:08:31 -0700

    Implemented login button styles

--- src/react/App.js
+++ src/react/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import { OctokitContext } from "@psw/contexts/github";
-import SignInButtons from "@psw/components/sign-in-buttons/sign-in-buttons";
 import AllPrsList from "@psw/components/all-prs-list/all-prs-list";
+import LoginContainer from "./components/login-container/login-container";
 
 function App() {
   const [authToken, setAuthToken] = useState("");
@@ -14,7 +14,7 @@ function App() {
           {authToken.length > 0 ? (
             <AllPrsList prs={prs} setPrs={setPrs}/>
           ) : (
-            <SignInButtons
+            <LoginContainer
               setAuthToken={setAuthToken}
               authToken={authToken}
               setPrs={setPrs}
--- src/react/components/sign-in-button/sign-in-button.js
+++ src/react/components/sign-in-button/sign-in-button.js
@@ -1,6 +1,9 @@
+import cs from "classnames";
+import styles from "./sign-in-button.module.scss";
+
 const SignInButton = ({ signinType }) => {
-  const { provider, onclickHandler } = signinType;
-  return <button onClick={onclickHandler}>{provider}</button>;
+  const { provider, onclickHandler, displayName } = signinType;
+  return <button className={cs(styles.buttonReset, styles.buttonBase, styles[`button${displayName}`])} onClick={onclickHandler}>Login with <i className={cs('fa-solid', 'fa-brands', `fa-${provider}`, styles.buttonProviderIcon)}></i><span className={cs(styles.buttonProviderName)}>{displayName}</span></button>;
 };
 
 export default SignInButton;
