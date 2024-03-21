cb0fc4c08082e08c2e83dbd3817d1db44411d27a
Antoinette <acj.gfx@gmail.com>
2024-03-21 09:48:37 -0700

    Updates

--- src/react/components/notification/notification.js
+++ src/react/components/notification/notification.js
@@ -2,28 +2,32 @@ import cs from "classnames";
 import styles from "./notification.module.scss";
 
 const Notification = ({ notificationDetails, isOnlyNotification, index }) => {
-    const notificationMap = {
-        "review_requested": {
-            displayName: "Review Requested"
-        },
-        "mentioned": {
-            displayName: "Mentioned"
-        },
-        "state_change": {
-            displayName: "Updated"
-        },
-        "approval_requested": {
-            displayName: "Approval Requested"
-        },
-        "team_mentioned": {
-            displayName: "Team Mention"
-        }
-    }
-    const isFirst = index === 0 && !isOnlyNotification;
-    return <p className={cs(styles.notification, styles[notificationDetails.reason], {
+  const notificationMap = {
+    review_requested: {
+      displayName: "Review Requested",
+    },
+    mentioned: {
+      displayName: "Mentioned",
+    },
+    state_change: {
+      displayName: "Updated",
+    },
+    approval_requested: {
+      displayName: "Approval Requested",
+    },
+    team_mentioned: {
+      displayName: "Team Mention",
+    },
+  };
+  return (
+    <p
+      className={cs(styles.notification, styles[notificationDetails.reason], {
         [styles.multipleNotifications]: !isOnlyNotification,
-        [styles.active]: isFirst
-    })}>{notificationMap[notificationDetails.reason].displayName}</p>
-}
+      })}
+    >
+      {notificationMap[notificationDetails.reason].displayName}
+    </p>
+  );
+};
 
 export default Notification;
--- src/react/components/notifications-list/notifications-list.js
+++ src/react/components/notifications-list/notifications-list.js
@@ -1,15 +1,54 @@
+import { useEffect, useState } from "react";
 import cs from "classnames";
 import Notification from "@psw/components/notification/notification";
 import styles from "./notifications-list.module.scss";
 
 const NotificationsList = ({ notifications }) => {
-    const isOnlyNotification = notifications.length === 1;
-    return <div className={cs(styles.notificationsListContainer)}>
-        <div className={cs(styles.notificationsList)}>{
-            notifications.map((notification, index )=> {
-                return <Notification notificationDetails={notification} isOnlyNotification={isOnlyNotification} index={index}/>
-            })}</div>
+  const isOnlyNotification = notifications.length === 1;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentNotification, setCurrentNotification] = useState(
+    notifications[currentIndex]
+  );
+
+  useEffect(() => {
+    if (!isOnlyNotification) {
+      const setNextNotification = setTimeout(() => {
+        const nextIndex = currentIndex + 1;
+        setCurrentIndex(nextIndex);
+        setCurrentNotification(notifications[nextIndex]);
+      }, 2000);
+      return () => clearTimeout(setNextNotification);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (currentIndex === notifications.length - 1) {
+      setTimeout(() => {
+        setCurrentIndex(0);
+        setCurrentNotification(notifications[0]);
+      }, 2000);
+    }
+  }, [currentIndex]);
+
+  return (
+    <div className={cs(styles.notificationsListContainer)}>
+      <div className={cs(styles.notificationsList)}>
+        <Notification
+          notificationDetails={currentNotification}
+          isOnlyNotification={isOnlyNotification}
+        />
+        {/* {notifications.map((notification, index) => {
+          return (
+            <Notification
+              notificationDetails={notification}
+              isOnlyNotification={isOnlyNotification}
+              index={index}
+            />
+          );
+        })} */}
+      </div>
     </div>
-}
+  );
+};
 
 export default NotificationsList;
