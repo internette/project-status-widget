ce2713061f4291420ef9e3abf66ecdf86361ce1e
Antoinette Janus <antoinettejanus@lululemon.com>
2024-03-22 17:50:07 -0700

    Set logic around icon

--- src/react/components/pr-item/pr-item.js
+++ src/react/components/pr-item/pr-item.js
@@ -20,6 +20,7 @@ const PrLineItem = ({ prDetails }) => {
   useEffect(() => {
     showNotificationsRef.current = notifications && notifications.length > 0;
   }, [notifications, notifications?.length]);
+  const showIcon = mergeableState.length > 0 && !isDraft;
 
   return (
     <li
@@ -34,7 +35,7 @@ const PrLineItem = ({ prDetails }) => {
               [styles.draft]: isDraft
             })}
           >
-            {mergeableState.length > 0 && (
+            {showIcon && (
               <i
                 className={cs("fa-solid", styles.listIcon, {
                   "fa-check": mergeableState === "clean",
